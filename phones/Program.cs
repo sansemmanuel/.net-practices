397a6ce411b56c47162fccc5062128182a1963e7
Emmanuel Sansberro <36830673+sansemmanuel@users.noreply.github.com>
2023-06-19 22:15:12 -0300

    Add files via upload

---
+++ phones/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+/*
+Crear algunos objetos en el main de Program y probar escribirle datos y mostrar en pantalla el estado del Telefono.
+Agregar método Llamar() sin parámetros que devuelva un string con la leyenda "Realizando llamada...".
+Sobrecargar el método Llamar(string contacto) para que reciba un contacto y devuelva un string con la leyenda "Llamando a " + contacto
+Probar métodos en el main mostrando en pantalla el comportamiento de los objetos.*/
+namespace phones
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Telefono[] telefonos = new Telefono[10];
+            for (int x = 0;x<10;x++)
+            {
+                Console.WriteLine("Ingrese Marca De Telefono...");
+                string marca = Console.ReadLine();
+                //telefonos[x] = new Telefono("Modelo X",marca);
+                Console.WriteLine("Ingrese Modelo...");
+                string modelo = Console.ReadLine();
+                //telefonos[x] = new Telefono("Marca X", modelo);
+                Console.WriteLine("Ingrese Codigo Operador 1,2 o 3...\n");
+                string codigoOperador = Console.ReadLine();
+                Console.WriteLine("Ingrese estado de llamada 1 positivo 0 falso");
+                string llamada = Console.ReadLine();
+                Console.WriteLine("Marca " + marca + " modelo " + modelo + " codigo operador " + codigoOperador + " realiza llamada? " + llamada);
+                                
+            }
+
+        }
+    }
+}
---
+++ phones/Telefono.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace phones
+{
+    class Telefono
+    {
+        //public int Llamar;
+        private string marca;
+        private int codigoOperador;
+        private int numeroTelefonico;
+        public string Modelo { get; }
+        public string Marca { get; }
+        public int NumeroTelefonico
+        {
+            get { return numeroTelefonico; }
+            set { numeroTelefonico = value; }
+        }
+        public int CodigoOperador
+        {
+            set
+            {
+                if (value <= 3)
+                {
+                    codigoOperador = value;
+                }
+                else
+                {
+                    codigoOperador = 0;
+                }
+
+            }
+            get { return codigoOperador; }
+        }
+        public Telefono()
+        {
+            Modelo = String.Empty;
+            marca = String.Empty;
+            NumeroTelefonico = 0;
+            CodigoOperador = 0;
+        }
+        public Telefono(string modelo, String marca)
+        {
+            Modelo = modelo;
+            this.marca = marca;
+            NumeroTelefonico = 0;
+            CodigoOperador = 0;
+        }
+        //metodo llamar()
+        public string Llamada()
+        {
+            if (codigoOperador == 1)
+            { return "realizando llamada..."; }
+            else
+            { return "no se puede realizar la llamda.."; }
+        }
+        /*public string Llamada(string contacto);
+        {
+        return "llamando a " + contacto;
+        }*/
+    }
+}
